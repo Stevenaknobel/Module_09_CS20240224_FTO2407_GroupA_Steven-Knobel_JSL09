023f6ceef9fd5654639b7e701645369fc01d752b
Steven <stevenaknobel@gmail.com>
2024-09-25 22:09:06 +0200

    create the flexbox and classes in HTML to space each element

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
+    //Challenge 2
+    //show the author's name on the page
+    document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+
+//Challenge 2
