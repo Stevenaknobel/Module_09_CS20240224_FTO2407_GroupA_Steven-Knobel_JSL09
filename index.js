b868ed400f9f88e9e3cf630ee432c96d082e694a
Steven <stevenaknobel@gmail.com>
2024-09-25 21:54:07 +0200

    created CSS, HTML and JS documents and added a dynamic background with JS

---
+++ index.js
@@ -0,0 +1,7 @@
+//Challenge 1
+//fetch a background image
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+    })
